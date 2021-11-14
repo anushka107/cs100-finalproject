d9159e0d4dc84a936b5e8494bed8aa27fef66bc8
karanvirchima <karanvirchima@gmail.com>
2021-11-14 13:03:44 -0800

    Working Aqua Man character class

---
+++ Aqua_Man.hpp
@@ -0,0 +1,24 @@
+#ifndef__AQUA_MAN__
+#define__AQUA_MAN__
+
+#include “character.hpp”
+
+class Aqua_Man : public Character {
+public:
+ virtual void attack(Character* character){
+     cout << "Aqua Man attacked with his water cannon  " << getAttackDamage() << " damage." << endl;
+     character->decreaseHealth(getAttackDamage());
+}
+virtual void specialAttack(Character* character){
+    cout << "Aqua Man attacked with his Trident and did " <<
+    getSpecialAttackDamage() << " damage." << endl;
+    character->decreaseHealth(getSpecialAttackDamage());
+}
+virtual void heal(Character* character){
+    cout << "Aqua Man chose to heal and added " <<
+    getHeal() << " health." << endl;
+    character->increaseHealth(getHeal());
+}
+};
+
+
