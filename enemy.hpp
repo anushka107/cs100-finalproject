c15e5ac3f1d51d9303d3742e4a570e48391111b8
anushka107 <anushkapandya5@gmail.com>
2021-11-22 12:10:09 -0800

    updated file

--- enemy.hpp
+++ enemy.hpp
@@ -1,52 +1,62 @@
+
+#pragma  once
+
 #include "character.hpp"
 
 class Enemy : public Character {
 
 public:
-    Enemy() : Character(10,5,5,20) {}
+  Enemy() : Character() {
+    generateRandomStats();
+  }
+  virtual void generateRandomStats() {
+    this->setAttackDamage(abs(rand() % 25));
+    this->setSpecialAttackDamage(abs(rand() % 25));
+    this->setHeal(abs(rand() % 25));
+    this->setHealth(abs(rand() % 25));
+  }
 
-   // 0 if enemy heals, nonzero if he attacks
-    void decideAction(Character * character) 
-    {
-     //   either attack, spec attack, or heal
-        int decision = rand() % 3;
-        switch (decision)
-        {
-        case ATTACK:
-            attack(character);
-            break;
-        case SPEC_ATTACK:
-            specialAttack(character);
-            break;
-        case HEAL:
-         //   setHealth(getHealth()+more_health);
-            heal(this);
-            break;
-        default:
-            break;
-        }
-    }
-    void attack(Character* character){
-        std::cout << "Enemy attacked with his shield toss and did " << getAttackDamage() << " damage." << std::endl;
-        character->decreaseHealth(getAttackDamage());
-    }
-    void specialAttack(Character* character){
-        std::cout << "Enemy attacked with his Hammer Combo and did " <<
-        getSpecialAttackDamage() << " damage." << std::endl;
-        character->decreaseHealth(getSpecialAttackDamage());
 
-    }
-    void heal(Character* character){
-        std::cout << "Enemy chose to heal and added " <<
-        getHeal() << " health." << std::endl;
-        character->increaseHealth(getHeal());
-    }
+  // 0 if enemy heals, nonzero if he attacks
+  virtual void decideAction(Character * character) 
+  {
+      either attac, spec attack, or heal
+      int decision = abs(rand() % 10);
+      switch (decision)
+      {// if you see errors here, it's because of the ... so use an if-else stmt
+      case 0 ... 3:
+        attack(character);
+        break;
+      case 4 ... 8:
+        heal(this);
+        break;
+      case 9:
+        specialAttack(character);
+        break;
+      default:
+        break;
+      }
+  }
+  virtual void attack(Character* character){
+      std::cout << "Enemy attacked with his shield toss and did " << getAttackDamage() << " damage." << std::endl;
+      character->decreaseHealth(getAttackDamage());
+  }
+  virtual void specialAttack(Character* character){
+      std::cout << "Enemy attacked with his Hammer Combo and did " <<
+      getSpecialAttackDamage() << " damage." << std::endl;
+      character->decreaseHealth(getSpecialAttackDamage());
+
+  }
+  virtual void heal(Character* character){
+      std::cout << "Enemy chose to heal and added " <<
+      getHeal() << " health." << std::endl;
+      character->increaseHealth(getHeal());
+  }
 private:
-    enum { 
-        ATTACK
-        SPEC_ATTACK,
-        HEAL
-    };
+  enum { // use to 
+      ATTACK,
+      SPEC_ATTACK,
+      HEAL
+  };
 };
-
 
