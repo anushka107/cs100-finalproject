2fbb20b6bd188a0b00f04a6aabd62e8a94aadb73
anushka107 <anushkapandya5@gmail.com>
2021-11-22 12:06:12 -0800

    updated file

--- Boss.hpp
+++ Boss.hpp
@@ -1,24 +1,61 @@
-nclude "enemy.hpp"
+#include "enemy.hpp"
+#include <string>
 
-class Boss : public Character
+class Boss : public Enemy
 {
 private:
-    /* data */
+    std::string name;
 public:
-    Boss();
-    Boss(int,int,int,int);
+    Boss(const std::string & bossName) {
+      name = bossName;
+      generateRandomStats();
+    }
     ~Boss();
+
+    std::string getName() const {
+      return name;
+    }
+
+    virtual void generateRandomStats() {
+      this->setAttackDamage(abs(rand() % 50));
+      this->setSpecialAttackDamage(50 + abs((rand() % 50)));
+      this->setHeal(abs(rand() % 100));
+      this->setHealth(abs(rand() % 100));
+    }
+
+    virtual void decideAction(Character * character) 
+    {
+     // either attac, spec attack, or heal
+      int decision = abs(rand() % 10);
+      switch (decision)
+      {// if you see errors here, it's because of the ... so use an if-else stmt
+      case 0 ... 2:
+        attack(character);
+        break;
+      case 4 ... 7:
+        heal(this);
+        break;
+      case 8 ... 9: // 20 % of time, boss does special
+        specialAttack(character);
+        break;
+      default:
+        break;
+      }
+    }
+    virtual void attack(Character* character){
+        std::cout << getName() << " attacked with his <ATTACK> and did " 
+                  << getAttackDamage() << " damage." << std::endl;
+        character->decreaseHealth(getAttackDamage());
+    }
+    virtual void specialAttack(Character* character){
+        std::cout << getName() << " attacked with his <SPEC ATTACK> and did " <<
+        getSpecialAttackDamage() << " damage." << std::endl;
+        character->decreaseHealth(getSpecialAttackDamage());
+
+    }
+    virtual void heal(Character* character){
+        std::cout << getName() << " chose to heal and added " <<
+        getHeal() << " health." << std::endl;
+        character->increaseHealth(getHeal());
+    }
 };
-
-Boss::Boss(/* args */) : Character(100,50,50,200)
-{
-}
-
- Boss::Boss(int ad,int sad,int heal,int health) 
- : Character( ad , sad, heal, health)
- {
- }
-
- Boss::~Boss()
- {
- }
