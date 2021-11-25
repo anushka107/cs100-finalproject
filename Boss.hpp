dab6c0eab9fcc43a05f49ae8fe5c8569931acd26
anushka107 <anushkapandya5@gmail.com>
2021-11-25 08:11:57 -0800

    changes

--- Boss.hpp
+++ Boss.hpp
@@ -1,61 +1,93 @@
-#include "enemy.hpp"
-#include <string>
-
-class Boss : public Enemy
+Class Boss
 {
 private:
-    std::string name;
+  int attackDamage;
+  int attackSpecialDamage;
+  int healthIncrease;// how much the character heals
+  int health;
+  int maxHealth;
+  int randomNumber(int low, int high);
+
 public:
-    Boss(const std::string & bossName) {
-      name = bossName;
-      generateRandomStats();
-    }
-    ~Boss();
+  Boss();
+  ~Boss()=default;
+  virtual void genRandomStats();
+  virtual void decideAction(Character *character);
+  virtual void attack(Character *character);
+  virtual void specialAttack(Character *character);
+  virtual void heal(Boss *character);
 
-    std::string getName() const {
-      return name;
-    }
+public: getters
+  virtual int getAttackDamage() const { return attackDamage; }
+  virtual int getSpecialAttackDamage() const { return attackSpecialDamage; }
+  virtual int getHealth() const { return health; }
+  virtual int getHeal() const { return healthIncrease; }
 
-    virtual void generateRandomStats() {
-      this->setAttackDamage(abs(rand() % 50));
-      this->setSpecialAttackDamage(50 + abs((rand() % 50)));
-      this->setHeal(abs(rand() % 100));
-      this->setHealth(abs(rand() % 100));
-    }
-
-    virtual void decideAction(Character * character) 
-    {
-     // either attac, spec attack, or heal
-      int decision = abs(rand() % 10);
-      switch (decision)
-      {// if you see errors here, it's because of the ... so use an if-else stmt
-      case 0 ... 2:
-        attack(character);
-        break;
-      case 4 ... 7:
-        heal(this);
-        break;
-      case 8 ... 9: // 20 % of time, boss does special
-        specialAttack(character);
-        break;
-      default:
-        break;
-      }
-    }
-    virtual void attack(Character* character){
-        std::cout << getName() << " attacked with his <ATTACK> and did " 
-                  << getAttackDamage() << " damage." << std::endl;
-        character->decreaseHealth(getAttackDamage());
-    }
-    virtual void specialAttack(Character* character){
-        std::cout << getName() << " attacked with his <SPEC ATTACK> and did " <<
-        getSpecialAttackDamage() << " damage." << std::endl;
-        character->decreaseHealth(getSpecialAttackDamage());
-
-    }
-    virtual void heal(Character* character){
-        std::cout << getName() << " chose to heal and added " <<
-        getHeal() << " health." << std::endl;
-        character->increaseHealth(getHeal());
-    }
+public:                                               // setters
+  virtual void resetHealth() { health = maxHealth; }; // set his health back to 0
+  virtual void decreaseHealth(int damage) { health -= damage; }
+  virtual void increaseHealth(int healing) { health += healing; }
 };
+
+Boss::Boss()
+{
+  genRandomStats();
+}
+
+void Boss::genRandomStats()
+{
+  attackDamage = abs(rand() % 2*25);
+  attackSpecialDamage = abs(rand() % 2*25);
+  healthIncrease = abs(rand() % 2*25);
+  health = abs(rand() % 2*25);
+  maxHealth = abs(rand() % 2*50);
+}
+
+ //0 if Boss heals, nonzero if he attacks
+void Boss::decideAction(Character *character)
+{
+ // either attac, spec attack, or heal
+  int decision = abs(rand() % 10);
+  switch (decision)
+  {//  if you see errors here, it's because of the ... so use an if-else stmt
+  case 0 ... 3:
+    attack(character);
+    break;
+  case 4 ... 8:
+    heal(this);
+    break;
+  case 9:
+    specialAttack(character);
+    break;
+  default:
+    break;
+  }
+}
+
+void Boss::attack(Character *character)
+{
+  int damage = randomNumber(getAttackDamage()-2,getAttackDamage()+2);
+  std::cout << "Boss attacked with his shield toss and did " << damage << " damage." << std::endl;
+  character->decreaseHealth(damage);
+}
+
+void Boss::specialAttack(Character *character)
+{
+  int damage = randomNumber(getAttackDamage()-4,getAttackDamage()+4);
+  std::cout << "Boss attacked with his Hammer Combo and did " << damage << " damage." << std::endl;
+  character->decreaseHealth(damage);
+}
+
+void Boss::heal(Boss *character)
+{
+  int healing = randomNumber(getHeal()-2,getHeal()+2);
+  std::cout << "Boss chose to heal and added " << getHeal() << " health." << std::endl;
+  character->increaseHealth(getHeal());
+}
+
+int Boss::randomNumber(int low, int high)
+{
+  srand(time(0));
+  return (rand() % (high - low + 1));
+}
+
