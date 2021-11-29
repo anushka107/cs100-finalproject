909996f2fc50777bfb998f4309fc33d5c70c05d2
karanvirchima <karanvirchima@gmail.com>
2021-11-29 10:28:24 -0800

    add Captain America

--- Captain_America.hpp
+++ Captain_America.hpp
@@ -1,27 +1,68 @@
-#ifndef __CAPTAIN_AMERICA__
-#define __CAPTAIN_AMERICA__
+#pragma once
 
-#include "character.hpp"
-#include <iostream>
-using namespace std;
+#include "Character.hpp"
+#include "Boss.hpp"
 
-class Captain_America : public Character {
+class Captain_America : public Character
+{
 public:
-  virtual void attack(Character* character){
-    cout << "Captain America attacked with his shield toss and did " << getAttackDamage() << " damage." << endl;
-    character->decreaseHealth(getAttackDamage());
-}
-  virtual void specialAttack(Character* character){
-    cout << "Captain America attacked with his Hammer Combo and did " <<
-    getSpecialAttackDamage() << " damage." << endl;
-    character->decreaseHealth(getSpecialAttackDamage());
-}
-  virtual void heal(Character* character){
-    cout << "Captain America chose to heal and added " <<
-    getHeal() << " health." << endl;
-    character->increaseHealth(getHeal());
-}
-};
+  Captain_America()
+  {
+    genRandomStats();
+  }
+  ~Captain_America()=default;
 
-#endif
+public:
+  virtual void genRandomStats()
+  {
+    name = "Captain America";
+    health = 200;
+    maxHealth = health;
+    setAttackDamage(rand() % 25 + rand() % 25);
+    setSpecialAttackDamage(rand() % 50 + rand() % 25);
+    setHeal(20 + rand() % 10);
+  }
+  virtual void attack(Character *character)
+  {
+    int damage = getAttackDamage();
+    Boss *bp = dynamic_cast<Boss *>(character);
+    if (bp != nullptr)
+    {
+      damage *= 2;
+      std::cout << "Captain America pulled out all the stops, and attacked with his shield toss and did " << damage << " damage." << std::endl;
+    }
+    else
+    {
+      std::cout << "Captain America attacked with his shield toss and did " << damage << " damage." << std::endl;
+    }
+
+    character->decreaseHealth(damage);
+    std::cout << "Enemy health = " << character->getHealth() << std::endl;
+  }
+  virtual void specialAttack(Character *character)
+  {
+    int damage = getSpecialAttackDamage();
+    Boss *bp = dynamic_cast<Boss *>(character);
+    if (bp != nullptr)
+    {
+      damage *= 2;
+      std::cout << "Captain America pulled out all the stops, and attacked with his Special Hammer Combo and did " << damage << " damage." << std::endl;
+    }
+    else
+    {
+      std::cout << "Captain America attacked with his Special Hammer Combo and did " << damage << " damage." << std::endl;
+    }
+
+    character->decreaseHealth(damage);
+    std::cout << "Enemy health = " << character->getHealth() << std::endl;
+  }
+  virtual void heal(Character *character)
+  {
+    std::cout << "Captain America chose to heal and added " << getHeal() << " health." << std::endl;
+    character->increaseHealth(getHeal());
+  }
+  virtual void resetHealth() { health = maxHealth; }; // set his health back to 0
+private:
+  int maxHealth;
+};
 
