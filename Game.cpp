0a2ad22c9a18d17086cacb1c5cf8354d663678a4
karanvirchima <91230405+karanvirchima@users.noreply.github.com>
2021-11-29 23:44:16 -0800

    Update Narrative

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,12 @@
 #include "GameHandler.hpp"
 int main()
 {
+    cout << "In our text-based game we will have three characters Aqua Man, Iron Man, and Captain America. The character you choose will have to face two battles." << endl << 
+        "The first battle will consist of a soldier of Thanos. If you have successfully beaten the soldier you will fight the boss, Thanos himself!!!" << endl << 
+        "Once you have arrived in battle, you will have the choice of the following: " << endl <<
+        "BasicAttack" << endl << " SpecialAttack( YOU ONLY ARE ALLOWED THREE OF THIS ATTACK PER BATTLE)" << endl << "Heal" << endl << "Quit the game( Please don't rage quit :). I assure you you will have a blast )"
+        << endl << "What are you waiting for? Go on an fight your journey!"
+        
     GameHandler game;
     game.chooseHero();
 
